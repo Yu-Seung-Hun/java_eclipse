72c411184fbd1cf01b933b8b37c89f24ec489d55
Yu-Seung-Hun <youu0208@naver.com>
2024-01-19 17:44:01 +0900

    file update

---
+++ hello_java_world/src/hello_java_world/Hello.java
@@ -0,0 +1,14 @@
+package hello_java_world;
+
+public class Hello {
+	
+	public static void sayHello() {
+		System.out.println("Hello!");
+	}
+	public static void main(String[] args) {
+		
+		sayHello();
+		
+	}
+
+}
