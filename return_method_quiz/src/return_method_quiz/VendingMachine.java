ac3794aea6726d41ff2e77b5be58950d93fd07e7
Yu-Seung-Hun <youu0208@naver.com>
2024-01-24 08:30:41 +0900

    Add code

---
+++ return_method_quiz/src/return_method_quiz/VendingMachine.java
@@ -0,0 +1,39 @@
+package return_method_quiz;
+
+public class VendingMachine {
+	
+	Product product;
+	
+	int money;
+	
+	
+	public VendingMachine() {
+		System.out.println("자판기 인스턴스를 만들었습니다!");
+		
+		this.product = new Product();
+		this.product.name = "제로콜라";
+		this.product.price = 1600;
+		this.product.quantity = 50;
+		
+		this.money = 100_000;
+		
+		
+	}
+	
+	
+	public void insertMoney(Customer customer) {
+		this.money += this.product.price;
+		customer.pay(this.product.price);
+	}
+	
+	
+	public void pressButton(Customer customer) {
+		if(this.product.quantity <= 0) {
+			return;
+		}
+		
+		this.product.quantity--;
+		customer.addStock(this.product.name, this.product.price);
+	}
+	
+}
