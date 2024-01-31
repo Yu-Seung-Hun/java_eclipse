7e3e712cffbf586af310ce9c7731e2adea670371
Seung Hun <youu0208@naver.com>
2024-01-31 17:49:33 +0900

    Add Exception Code

--- exception_handling/src/IfNullPointerException.java
+++ exception_handling/src/IfNullPointerException.java
@@ -1,4 +1,18 @@
 
 public class IfNullPointerException {
-
+	
+	public static void main(String[] args) {
+		
+		String name = null;
+		
+		// NullPointerException - null.equals("이름") 
+		// NullPointerException 방지방법
+		if(name != null) {
+			System.out.println(name.equals("이름"));
+		}
+		
+		System.out.println("name is null");
+		
+	}
+	
 }
--- exception_handling/src/IfNumberFormatException.java
+++ exception_handling/src/IfNumberFormatException.java
@@ -1,4 +1,22 @@
 
 public class IfNumberFormatException {
-
+	
+	public static void main(String[] args) {
+		
+		String numString = "123";
+		
+		// NumberFormatException 방지방법 - 변환 전 숫자의 형태를 체크
+		if(numString.matches("^[0-9]+$")) {
+			int num = Integer.parseInt(numString);
+			System.out.println(num); // 123
+		}
+		
+		numString = "ABC";
+		if(numString.matches("^[0-9]+$")) {
+			int num = Integer.parseInt(numString); // NumberFormatException
+			System.out.println(num);
+		}
+		
+	}
+	
 }
