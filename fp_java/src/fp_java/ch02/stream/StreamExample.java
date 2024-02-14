cdd5f6a41378872c6f01c5897773c050737600ad
Seung Hun <youu0208@naver.com>
2024-02-14 17:51:39 +0900

    code update

---
+++ fp_java/src/fp_java/ch02/stream/StreamExample.java
@@ -0,0 +1,12 @@
+package fp_java.ch02.stream;
+
+import java.util.List;
+
+import fp_java.ch02.stream.vo.Dish;
+import fp_java.ch02.stream.vo.DishData;
+
+public class StreamExample {
+	
+	List<Dish> menu = DishData.menu;
+	
+}
