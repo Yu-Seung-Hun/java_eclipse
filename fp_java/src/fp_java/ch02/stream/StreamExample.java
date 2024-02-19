ea280af6da4735b6b1472a126ee44a644d4da6b1
Seung Hun <youu0208@naver.com>
2024-02-19 21:57:23 +0900

    code update

--- fp_java/src/fp_java/ch02/stream/StreamExample.java
+++ fp_java/src/fp_java/ch02/stream/StreamExample.java
@@ -1,6 +1,7 @@
 package fp_java.ch02.stream;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import fp_java.ch02.stream.vo.Dish;
 import fp_java.ch02.stream.vo.DishData;
@@ -8,5 +9,10 @@ import fp_java.ch02.stream.vo.DishData;
 public class StreamExample {
 	
 	List<Dish> menu = DishData.menu;
+	
+	List<String> lowCaloricDishesName = menu.stream()
+											.filter( (dish) -> dish.getCalories() < 400)
+											.map( (dish) -> dish.getName())
+											.collect(Collectors.toList());
 	
 }
