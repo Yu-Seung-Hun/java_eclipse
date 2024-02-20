a9700fbb337dcc7d0e1f1c07e2c7282408abf597
Seung Hun <youu0208@naver.com>
2024-02-20 17:40:48 +0900

    Add SlicingExample code

--- fp_java/src/fp_java/ch02/stream/StreamExample.java
+++ fp_java/src/fp_java/ch02/stream/StreamExample.java
@@ -1,5 +1,6 @@
 package fp_java.ch02.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +9,20 @@ import fp_java.ch02.stream.vo.DishData;
 
 public class StreamExample {
 	
-	List<Dish> menu = DishData.menu;
-	
-	List<String> lowCaloricDishesName = menu.stream()
-											.filter( (dish) -> dish.getCalories() < 400)
-											.map( (dish) -> dish.getName())
-											.collect(Collectors.toList());
+	public static void main(String[] args) {
+		List<Dish> menu = DishData.menu;
+		
+		List<String> lowCaloricDishesName = 
+				menu.stream()
+				.filter( (dish) -> dish.getCalories() > 300)
+				.sorted(Comparator.comparing(Dish::getCalories))
+				.map(Dish::getName)
+				.skip(2)
+				.limit(3)
+				.collect(Collectors.toList());
+		
+		lowCaloricDishesName
+			.forEach(System.out::println);
+	}
 	
 }
